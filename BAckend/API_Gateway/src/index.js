6150caed47256c4fc074e11f58a064447fc3677c
Shubh7amydv <shubham.2023ug1109@iiitranchi.ac.in>
2026-07-17 15:33:07 +0530

    Enable CORS on API Gateway for frontend client integration

--- BAckend/API_Gateway/src/index.js
+++ BAckend/API_Gateway/src/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { PORT } = require('./config/serverconfig');
+const cors = require('cors');
 const apiRoutes = require('./routes');
 
 const setupAndStartServer = () => {
     const app = express();
     
+    app.use(cors());
     app.use(morgan('combined'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
